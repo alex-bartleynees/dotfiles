aa6ab0fb0b00ffc27157d1041aa1677f2d8a7adc
Alex Bartley Nees <alexbartleynees@gmail.com>
2024-07-06 15:28:22 +1200

    Update i3 config

--- configs/i3/lock.sh
+++ configs/i3/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 xset s off dpms 0 10 0
-i3lock -i ~/dotfiles/backgrounds/Clearnight.png -u --ignore-empty-password --show-failed-attempts --nofork
+i3lock -i ~/dotfiles/backgrounds/arnold-dogelis-9ftemxDaNXQ-unsplash.jpg -u --ignore-empty-password --show-failed-attempts --nofork
 xset s off -dpms
