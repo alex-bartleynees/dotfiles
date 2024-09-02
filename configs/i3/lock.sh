0c1abbd45efe62dbf0d5d2a46344f87371a82d40
Alex Bartley Nees <alexbartleynees@gmail.com>
2024-09-02 19:29:36 +1200

    Update i3 config

--- configs/i3/lock.sh
+++ configs/i3/lock.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 xset s off dpms 0 10 0
-i3lock -i ~/dotfiles/backgrounds/arnold-dogelis-9ftemxDaNXQ-unsplash.jpg -u --ignore-empty-password --show-failed-attempts --nofork
+i3lock -i ~/dotfiles/backgrounds/lockscreen_composite.png -u --ignore-empty-password --show-failed-attempts --nofork
 xset s off -dpms
+exec xrandr --output DP-4 --rotate right --right-of DP-0 --auto
