83d5592d545e1d88da22e4c92ef99e0fcd07040a
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-07-01 07:35:43 +1200

    Update powermenu and waybar

--- configs/rofi/powermenu/type-1/powermenu.sh
+++ configs/rofi/powermenu/type-1/powermenu.sh
@@ -57,42 +57,86 @@ run_rofi() {
 	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
+# Detect current compositor
+get_compositor() {
+    if [[ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]]; then
+        echo "hyprland"
+    elif [[ "$XDG_CURRENT_DESKTOP" == "sway" ]] || pgrep -x sway > /dev/null; then
+        echo "sway"
+    else
+        echo "unknown"
+    fi
+}
+
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-      killall sway 
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        compositor="$(get_compositor)"
+        
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--suspend' ]]; then
+            mpc -q pause
+            amixer set Master mute
+            systemctl suspend
+        elif [[ $1 == '--logout' ]]; then
+            case "$compositor" in
+                "hyprland")
+                    hyprctl dispatch exit
+                    ;;
+                "sway")
+                    killall sway
+                    ;;
+                *)
+                    echo "Unknown compositor, cannot logout"
+                    exit 1
+                    ;;
+            esac
+        fi
+    else
+        exit 0
     fi
-	else
-		exit 0
-	fi
+}
+
+# Execute lock command based on compositor
+run_lock() {
+    compositor="$(get_compositor)"
+    case "$compositor" in
+        "hyprland")
+            hyprlock
+            ;;
+        "sway")
+            if [[ -f ~/.config/dotfiles/configs/sway/lock.sh ]]; then
+                ~/.config/dotfiles/configs/sway/lock.sh
+            else
+                echo "Sway lock script not found"
+            fi
+            ;;
+        *)
+            echo "Unknown compositor, cannot lock"
+            ;;
+    esac
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
     $shutdown)
-		run_cmd --shutdown
+        run_cmd --shutdown
         ;;
     $reboot)
-		run_cmd --reboot
+        run_cmd --reboot
         ;;
     $lock)
-    ~/.config/dotfiles/configs/sway/lock.sh
+        run_lock
         ;;
     $suspend)
-		run_cmd --suspend
+        run_cmd --suspend
         ;;
     $logout)
-		run_cmd --logout
+        run_cmd --logout
         ;;
 esac
