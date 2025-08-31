a8c0d437f712be568c4f072462a15b0ff4198d61
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-08-31 20:10:08 +1200

    Add theme colors for wallpaper selector

--- configs/rofi-custom/wallpaper.sh
+++ configs/rofi-custom/wallpaper.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
-WallDir=${1:-~/.config/dotfiles/backgrounds}
+# Usage: wallpaper.sh [theme] [wallpaper_dir]
+# Themes: catppuccin-mocha, tokyo-night, everforest
+
+THEME=${1:-catppuccin-mocha}
+WallDir=${2:-~/.config/dotfiles/backgrounds}
+
+# Map theme name to theme file
+get_theme_file() {
+    case "$1" in
+        "catppuccin-mocha"|"mocha")
+            echo "fullscreen-preview-catppuccin-mocha.rasi"
+            ;;
+        "tokyo-night"|"tokyo")
+            echo "fullscreen-preview-tokyo-night.rasi"
+            ;;
+        "everforest"|"forest")
+            echo "fullscreen-preview-everforest.rasi"
+            ;;
+        *)
+            echo "fullscreen-preview-catppuccin-mocha.rasi"
+            ;;
+    esac
+}
+
+THEME_FILE=$(get_theme_file "$THEME")
 
 PREVIEW=true \
-rofi -no-config -theme fullscreen-preview-custom.rasi \
+rofi -no-config -theme "$THEME_FILE" \
     -show filebrowser -filebrowser-command 'swww img' \
     -filebrowser-directory "$WallDir" \
     -filebrowser-sorting-method mtime \
