cac97f2b175c5645b2664bbcc58de3cc861aa3cd
Alex Bartley Nees <alexbartleynees@gmail.com>
2026-02-10 07:31:08 +1300

    Add gruvbox nvim theme

--- themes/gruvbox/nvim/colorscheme.lua
+++ themes/gruvbox/nvim/colorscheme.lua
@@ -1,13 +1,10 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "ellisonleao/gruvbox.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("tokyonight").setup({
-        style = "night",
-      })
       -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme gruvbox]])
     end,
   },
 }
