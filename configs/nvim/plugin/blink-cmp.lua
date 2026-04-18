07414f684805362c167828d31f9e6df1af00d7fa
Alex Bartley Nees <alexbartleynees@gmail.com>
2026-04-19 00:57:36 +1200

    update blink

--- configs/nvim/plugin/blink-cmp.lua
+++ configs/nvim/plugin/blink-cmp.lua
@@ -21,6 +21,8 @@ require("lazyload").on_vim_enter(function()
       preset = "none",
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-f>"] = { "scroll_documentation_down", "fallback" },
       ["<C-Space>"] = { "show", "fallback" },
--- configs/nvim/plugin/roslyn.lua
+++ configs/nvim/plugin/roslyn.lua
@@ -3,14 +3,7 @@ require("lazyload").on_vim_enter(function()
     { src = "https://github.com/seblyng/roslyn.nvim" },
   })
 
-  local ok, blink = pcall(require, "blink.cmp")
-  local capabilities = ok and blink.get_lsp_capabilities() or vim.lsp.protocol.make_client_capabilities()
-
   if os.getenv("DOTNET_ROOT") or vim.fn.executable("dotnet") == 1 then
-    vim.lsp.config("roslyn", {
-      capabilities = capabilities,
-    })
-
     require("roslyn").setup({
       broad_search = true,
       lock_target = false,
